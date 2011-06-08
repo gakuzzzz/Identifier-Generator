956136b91c198f77a5e7a18fbb77ea545368aa19
gakuzzzz <gaku_github@t2v.jp>
2011-06-08 21:36:35 +0900

    重複する可能性が残るならRandomUUIDでいいので、重複がおこらないように修正

--- src/main/scala/jp/t2v/util/identify/IdentifierGenerator.scala
+++ src/main/scala/jp/t2v/util/identify/IdentifierGenerator.scala
@@ -1,30 +1,62 @@
 package jp.t2v.util.identify
 
+import collection.mutable.StringBuilder
+import annotation.tailrec
+
 class IdentifierGenerator private (elements: IdentifierElement[_]*) {
 
-  def fold(t: Seq[_]): Long = t.foldLeft(17L) { (a, b) => a * 2909 + hash(b) }
-  
-  def apply(): String = fold(elements.map(_())).toHexString
+  private val table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".toCharArray
+  private val mask = 0x3f
+  private val bitPerChar = 6
 
-  type ToLong = {def toLong: Long}
-  
-  def hash(value: Any): Long = value match {
-    case null => 0
-    case l: Long => l
-    case a: Array[_] => fold(a)
-    case t: ToLong => t.toLong
-    case n => n.hashCode
+  def apply(): String = fold(elements.map(_()))
+
+  private def fold(t: Seq[_]): String = {
+    val encoded = new StringBuilder
+    val (rr, ss) = t.foldLeft((0, 0)) { case ((r, s), v) => encode(v, r, s, encoded) }
+    if (ss > 0) encoded += table(rr & mask)
+    encoded.toString()
   }
-  
+
+  private def encode(value: Any, previous: Int, residualBitSize: Int, encoded: StringBuilder): (Int, Int) = value match {
+    case l: Long =>
+      val (r, s) = encode(l.toInt, 32, previous, residualBitSize, encoded)
+      encode((l >>> 32).toInt, 32, r, s, encoded)
+    case i: Int => encode(i, 32, previous, residualBitSize, encoded)
+    case a: Array[Byte] => a.foldLeft((previous, residualBitSize)) {
+      case ((r, s), v) => { encode(0xff & v, 8, r, s, encoded) }
+    }
+  }
+
+  private def encode(current: Int, currentBitSize: Int, previous: Int, residualBitSize: Int, encoded: StringBuilder): (Int, Int) = {
+      encoded += table(((current << residualBitSize) | previous) & mask)
+      val used = bitPerChar - residualBitSize
+      encode(current >>> used, currentBitSize - used, encoded)
+  }
+
+  @tailrec
+  private def encode(bits: Int, size: Int, encoded: StringBuilder): (Int, Int) = {
+    if (size < bitPerChar) {
+      (bits, size)
+    } else {
+      encoded += table(bits & mask)
+      encode(bits >>> bitPerChar, size - bitPerChar, encoded)
+    }
+  }
+
 }
 
-object IdentifierGenerator {
+object IdentifierGenerator extends (() => String) {
 
   def createInstance(elements: IdentifierElement[_]*) = new IdentifierGenerator(elements: _*)
-  
+
   @volatile
-  lazy val standard = createInstance(RandomInt, LocalIp, ThreadId, MilliTime, NanoTime)
-  
+  private lazy val standard = createInstance(RandomInt, NanoTime, MilliTime, ThreadId, ProcessId, LocalIp)
+
   def apply(): String = standard()
-  
+
+  def main(args: Array[String]) {
+    println(IdentifierGenerator())
+  }
+
 }
--- src/main/scala/jp/t2v/util/identify/elements.scala
+++ src/main/scala/jp/t2v/util/identify/elements.scala
@@ -1,30 +1,29 @@
 package jp.t2v.util.identify
 
-trait IdentifierElement[A] {
+trait IdentifierElement[A]  {
   def apply(): A
 }
 
 object ThreadId extends IdentifierElement[Long] {
-  def apply = Thread.currentThread.getId
+  def apply() = Thread.currentThread.getId
 }
 
 object MilliTime extends IdentifierElement[Long] {
-  def apply = System.currentTimeMillis
+  def apply() = System.currentTimeMillis
 }
 
-object NanoTime extends IdentifierElement[Long] {
-  def apply = System.nanoTime
+object NanoTime extends IdentifierElement[Int] {
+  def apply() = System.nanoTime.toInt
 }
 
-object RandomInt extends IdentifierElement[Long] {
+object RandomInt extends IdentifierElement[Int] {
   import util.Random
 
   val random = new ThreadLocal[Random] {
     override def initialValue = new Random
   }
   
-  def apply = random.get.nextLong
- 
+  def apply() = random.get.nextInt()
 }
 
 object LocalIp extends IdentifierElement[Array[Byte]] {
@@ -32,5 +31,11 @@ object LocalIp extends IdentifierElement[Array[Byte]] {
 
   @volatile
   lazy val apply = getLocalHost.getAddress
+}
 
+object ProcessId extends IdentifierElement[Int] {
+  import java.lang.management.ManagementFactory._
+
+  @volatile
+  lazy val apply = getRuntimeMXBean.getName.takeWhile(_ != '@').toInt
 }
